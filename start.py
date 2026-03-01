b3b5644635eee817aba446623c3ce8db312d59c9
Capybara <KapibarovKonstantin@gmail.com>
2026-03-01 18:57:55 +0400

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask('')
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def home():
+    return render_template("website/index.html")
+
+if __name__ == '__main__':
+  run()
