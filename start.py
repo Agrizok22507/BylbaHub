623b9056b4191ee427e1745df26d8c0ea72dea5b
Capybara <KapibarovKonstantin@gmail.com>
2026-03-01 19:44:27 +0400

    Update start.py

--- start.py
+++ start.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+from flask_cors import CORS
+import os
 
-app = Flask('')
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
 if __name__ == '__main__':
-  run()
+    app.run(host='0.0.0.0', port=8080)
