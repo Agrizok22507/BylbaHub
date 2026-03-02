0cecd6f6937f33812824b89fff87e85bfd77decc
Capybara <KapibarovKonstantin@gmail.com>
2026-03-02 17:22:47 +0400

    Update bylbahub.js

--- static/bylbahub.js
+++ static/bylbahub.js
@@ -15,124 +15,206 @@ function notification(text) {
     }, 3000);
 }
 
-function setTheme(color1, color2, color3) {
-    styleText = `
-    @keyframes gradientMove {
-        0% { background-position: 0% 0%; }
-        50% { background-position: 0% 100%; }
-        100% { background-position: 0% 0%; }
-    }
-
-    @keyframes animateBorder {
-        0% { border: 3px solid ${color1}; }
-        25% { border: 3px solid ${color2}; }
-        50% { border: 3px solid ${color3}; }
-        75% { border: 3px solid ${color2}; }
-        100% { border: 3px solid ${color1}; }
-    }
-
-    button:hover {
-        background: #666;
-        border-color: #999;
-        transform: scale(1.02) rotate(15deg);
-    }
-    `
-
-    const elementsWithBorder = ['BylbaConsoleMenu', 'BylbaConsolebtn', 'BylbaConsolebtnExecute', 'BylbaConsolebtnPanic', 'BylbaConsolebtnCopy', 'BylbaConsolebtnPaste', 'BylbaConsolebtnClear'];
+function setTheme(color) {
+    const elementsWithBorder = ['BylbaHubButton', 'BylbaHubMenu', 'BylbaHubButtonHub', 'BylbaHubButtonTheme', 'BylbaHubButtonPanic', 'BylbaHubLabel', 'BylbaHubStyle'];
     elementsWithBorder.forEach(currentElementForToChangeBorder=>{
         var element = document.getElementById(currentElementForToChangeBorder);
-        element.style.border = color1;
-        element.animation = 'animateBorder 3s ease infinite;';
+        element.style.border = color;
     });
-    document.getElementById('BylbaConsoleStyle').textContent = styleText;
-    document.getElementById('BylbaConsoleLabel').style.cssText = `font-size: 35px; background: linear-gradient(180deg, ${color1}, ${color2}, ${color3}); background-size: 100% 300%; -webkit-background-clip: text; background-clip: text; color: transparent; animation: gradientMove 3s ease infinite; display: inline-block; font-weight: bold;`;
+    
+    document.getElementById('BylbaHubStyle').textContent = `
+    button:hover {
+        background: #666;
+        border-color: ${color};
+    }
+    `;
 }
 
 function toggleBylbaHub() {
-    if (document.getElementById('BylbaConsoleMenu').style.display === 'none') {
-        document.getElementById('BylbaConsoleMenu').style.display = 'block';
+    if (document.getElementById('BylbaHubMenu').style.display === 'none') {
+        document.getElementById('BylbaHubMenu').style.display = 'block';
     } else {
-        document.getElementById('BylbaConsoleMenu').style.display = 'none';
+        document.getElementById('BylbaHubMenu').style.display = 'none';
     }
 }
 
 function panicMode() {
-    navigator.clipboard.writeText("fetch('https://raw.githubusercontent.com/Agrizok22507/BylbaHub/main/main/main.js').then(r=>r.text()).then(eval)");
+    navigator.clipboard.writeText("fetch('https://bylbahub.onrender.com/static/main.js').then(r=>r.text()).then(eval)");
     
-    var elementsToRemove = ['BylbaConsoleMenu', 'BylbaConsolebtn', 'BylbaConsoleLabel', 'BylbaConsoleInput', 'BylbaConsolebtnExecute', 'BylbaConsolebtnPanic', 'BylbaConsolebtnCopy', 'BylbaConsolebtnPaste', 'BylbaConsolebtnClear'];
+    var elementsToRemove = ['BylbaHubButton', 'BylbaHubMenu', 'BylbaHubButtonHub', 'BylbaHubButtonTheme', 'BylbaHubButtonPanic', 'BylbaHubLabel', 'BylbaHubStyle'];
     elementsToRemove.forEach(currentElementToRemove => {document.getElementById(currentElementToRemove)?.remove();});
 
     notification("Panic mode: ON");
 }
 
+function appendExtension(name, version, link, importLink, author, description) {
+    const divExtension = document.createElement('div');
+    const divTitle = document.createElement('div');
+    const titleExtension = document.createElement('span');
+    const versionTitleExtenstion = document.createElement('span');
+    const divAbout = document.createElement('div');
+    const aboutExtension = document.createElement('span');
+    const buttonInstall = document.createElement('button');
+
+    const menu = document.getElementById('BylbaHubMenu');
+
+    divExtension.style.cssText = 'width: 33%; height: 98%;';
+    versionTitleExtenstion.style.cssText = 'color: #999;';
+
+    titleExtension.className = 'bylbahublabel';
+    aboutExtension.className = 'bylbahublabel';
+    buttonInstall.className = 'bylbahubbutton';
+
+    titleExtension.innerText = name;
+    versionTitleExtenstion.innerText = `(v${version})`
+    aboutExtension.innerText = `Description: ${description}\nAuthor: ${author}\nWebsite: ${link}`;
+
+    buttonInstall.onclick=()=>`const script = document.createElement('script);
+    const resp = await fetch(${importLink});
+
+    script.textContent = resp;
+    document.head.appendChild(script);`;
+
+    divTitle.appendChild(titleExtension);
+    divTitle.appendChild(versionTitleExtenstion);
+
+    divAbout.appendChild(aboutExtension);
+
+    divExtension.appendChild(divTitle);
+    divExtension.appendChild(divAbout);
+    divExtension.appendChild(buttonInstall);
+
+    menu.appendChild(divExtension);
+}
+
+function changeBylbaHubMenu(menu) {
+    if (menu === 'Hub') {
+        const rgr = '';
+    }
+}
+
 function initilizeBylbaHub() {
     const button = document.createElement('button');
     const menu = document.createElement('div');
+    const labelDiv = document.createElement('div');
+    const label1 = document.createElement('span')
+    const label2 = document.createElement('span')
+    const buttonsDiv = document.createElement('div');
     const buttonHub = document.createElement('button');
     const buttonTheme = document.createElement('button');
     const buttonPanic = document.createElement('button');
-    const label = document.createElement('span')
+    const contentDiv = document.createElement('div');
     const style = document.createElement('style');
 
     button.id = 'BylbaHubButton';
     menu.id = 'BylbaHubMenu';
+    labelDiv.id = 'BylbaHubLabelDiv';
+    label1.id = 'BylbaHubLabel1';
+    label2.id = 'BylbaHubLabel2';
+    buttonsDiv.id = 'BylbaHubButtonsDiv';
     buttonHub.id = 'BylbaHubButtonHub';
     buttonTheme.id = 'BylbaHubButtonTheme';
     buttonPanic.id = 'BylbaHubButtonPanic';
-    label.id = 'BylbaHubLabel';
+    contentDiv.id = 'BylbaHubContentDiv';
     style.id = 'BylbaHubStyle';
 
-    button.style.cssText = 'position: fixed; font-size: 25px; background: #333; color: white; border: 3px solid #666; top: 15px; left: 15px; border-radius: 7px; margin: 6px; animation: animateBorder 3s ease infinite;';
-    menu.style.cssText = 'display: none; position: fixed; background: #333; border: 3px solid #9e34eb; border-radius: 10px; top: 50%; left: 50%; transform: translate(-50%, -50%); height: 200px; width: 600px; padding: 20px; animation: animateBorder 3s ease infinite;';
-    buttonHub.style.cssText = 'font-size: 35px; background: #333; color: white; border: 3px solid #666; top: 20px; left: 50px; border-radius: 7px; margin: 6px; transition: rotate 0.3s ease, scale 0.3s ease, border-color 0.3s ease, background 0.3s ease; animation: animateBorder 3s ease infinite;';
-    buttonTheme.style.cssText = 'font-size: 35px; background: #333; color: white; border: 3px solid #666; top: 20px; left: 50px; border-radius: 7px; margin: 6px; transition: rotate 0.3s ease, scale 0.3s ease, border-color 0.3s ease, background 0.3s ease; animation: animateBorder 3s ease infinite;';
-    buttonPanic.style.cssText = 'font-size: 35px; background: #333; color: white; border: 3px solid #666; top: 20px; left: 50px; border-radius: 7px; margin: 6px; transition: rotate 0.3s ease, scale 0.3s ease, border-color 0.3s ease, background 0.3s ease; animation: animateBorder 3s ease infinite;';
-    label.style.cssText = `font-size: 35px; background: linear-gradient(180deg, #9e34eb, #eb34a4, #34ebd8); background-size: 100% 300%; -webkit-background-clip: text; background-clip: text; color: transparent; animation: gradientMove 3s ease infinite; display: inline-block; font-weight: bold;`;
+    button.className = 'bylbahubbutton';
+    labelDiv.className = 'bylbahubdiv';
+    label1.className = 'bylbahublabel';
+    label2.className = 'bylbahublabel';
+    buttonsDiv.className = 'bylbahubdiv';
+    buttonHub.className = 'bylbahubbutton';
+    buttonTheme.className = 'bylbahubbutton';
+    buttonPanic.className = 'bylbahubbutton';
+    contentDiv.className = 'bylbahubdiv';
 
     button.innerText = 'BylbaHub';
     buttonHub.innerText = 'Hub';
     buttonTheme.innerText = 'Themes';
     buttonPanic.innerText = 'Panic';
-    label.innerText = 'BylbaHub';
+    label1.innerText = 'Bylba';
+    label2.innerText = 'Hub';
 
     style.textContent = `
-    @keyframes gradientMove {
-        0% { background-position: 0% 0%; }
-        50% { background-position: 0% 100%; }
-        100% { background-position: 0% 0%; }
+    .bylbahubbutton {
+        font-size: 35px;
+        background: #333;
+        color: white;
+        border: 3px solid #666;
+        top: 20px;
+        left: 50px;
+        border-radius: 7px;
+        margin: 6px;
+        transition: border-color 0.1s ease,;
     }
 
-    @keyframes animateBorder {
-        0% { border: 3px solid #9e34eb; }
-        25% { border: 3px solid #eb34a4; }
-        50% { border: 3px solid #34ebd8; }
-        75% { border: 3px solid #eb34a4; }
-        100% { border: 3px solid #9e34eb; }
+    .bylbahubbutton:hover {
+        border-color: #cc2b2b;
     }
 
-    button:hover {
-        background: #666;
-        border-color: #999;
-        transform: scale(1.02) rotate(15deg);
+    #BylbaHubButton {
+        position: fixed;
+        left: 2%;
+        top: 4%;
+    }
+
+    .bylbahubdiv {
+        height: 13%;
+        width: 98%;
+        margin: 1%;
+    }
+
+    #BylbaHubLabelDiv BylbaHubButtonsDiv {
+        height: 13%;
+    }
+
+    #BylbaHubButtonsDiv {
+        height: 74%;
+    }
+
+    #BylbaHubMenu {
+        display: none;
+        position: fixed;
+        background: #333;
+        border: 3px solid #cc2b2b;
+        border-radius: 10px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 35%;
+        width: 50%;
+        padding: 5px;
+    }
+
+    .bylbahublabel {
+        font-size: 35px;
+        display: inline-block;
+        font-weight: bold;
+    }
+
+    #BylbaHubLabel1 {
+        color: #ffffff;
+    }
+
+    #BylbaHubLabel2 {
+        color: #cc2b2b;
     }
     `;
 
     button.onclick=()=>toggleBylbaHub();
-    buttonHub.onclick=()=>panicMode();
-    buttonTheme.onclick=()=>panicMode();
+    buttonHub.onclick=()=>changeBylbaHubMenu('Hub');
+    buttonTheme.onclick=()=>changeBylbaHubMenu('Theme');
     buttonPanic.onclick=()=>panicMode();
 
-    fetch('https://raw.githubusercontent.com/Agrizok22507/BylbaHub/main/main/background.jpg').then(r=>r.blob()).then(b=>{
-    const url = URL.createObjectURL(b);
-    menu.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.3)), url('${url}')`;
-    menu.style.backgroundSize = 'cover';
-    menu.style.backgroundPosition = 'center';
-    menu.style.backgroundRepeat = 'no-repeat';}).catch(console.error);
+    buttonsDiv.appendChild(buttonHub);
+    buttonsDiv.appendChild(buttonTheme);
+    buttonsDiv.appendChild(buttonPanic);
+    labelDiv.appendChild(label1);
+    labelDiv.appendChild(label2);
 
-    menu.appendChild(buttonHub);
-    menu.appendChild(buttonTheme);
-    menu.appendChild(buttonPanic);
-    menu.appendChild(label);
+    menu.appendChild(labelDiv);
+    menu.appendChild(buttonsDiv);
+    menu.appendChild(contentDiv);
 
     document.head.appendChild(style);
     document.body.appendChild(button);
